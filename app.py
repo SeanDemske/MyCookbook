ed72dd27f4205802d5dd65aadd65151ebcfcaf42
Sean Demske <seandemske@gmail.com>
2020-11-24 16:05:26 -0500

    UI for recipe cards

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -11,3 +13,43 @@ def homepage():
     """Show homepage"""
 
     return render_template("index.html")
+
+@app.route("/browse")
+def browse():
+    """Testing displayed collection"""
+
+    resp = requests.get(
+            "https://api.edamam.com/search",
+            params={
+                "q": "chicken",
+                "app_id": "85ac62e0",
+                "app_key": "6b7cae050360bfe22e3783935fba9187",
+                "to": "20"
+            }
+        )
+    
+    results = resp.json()["hits"]
+
+    return render_template("search_results.html", results=results)
+
+@app.route("/ui")
+def ui():
+    """Testing UI"""
+
+    return render_template("search_results.html")
+
+# /search?q="chicken" GET
+
+# /profile/<username> GET
+
+# /profile/<username>/add GET/POST
+
+# /recipes/<recipe_id> GET
+
+# /recipes/<recipe_id>/add POST
+
+# /signup GET/POST
+
+# /signin GET/POST
+
+
